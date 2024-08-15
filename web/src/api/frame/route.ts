074c0d39acbbfcfd27ee751bd5ce8dd9353c0aef
temitopeohassan <temitope.hassan.2@gmail.com>
2024-08-15 21:32:08 +0100

    correction

--- web/src/api/frame/route.ts
+++ web/src/api/frame/route.ts
@@ -1,6 +1,6 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
-import { NEXT_PUBLIC_URL } from '../../config';
+import { NEXT_PUBLIC_URL } from '../../app/config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
